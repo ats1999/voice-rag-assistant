65ff906ccf78eb6f240cc8f6996a2588a593f52a
ats1999 <rkt82224@gmail.com>
2025-09-28 21:42:06 +0530

    added metric module in vector db service

--- src/vector-db/vector-db.module.ts
+++ src/vector-db/vector-db.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VectorDBService } from './vector-db.service';
 import { ConfigModule } from '@nestjs/config';
+import { MetricsModule } from 'src/metrics/metric.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, MetricsModule],
   providers: [VectorDBService],
   exports: [VectorDBService],
 })
