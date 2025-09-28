2100c3beecc7c4d6f5b928db4fd2f5bca642f000
ats1999 <rkt82224@gmail.com>
2025-09-28 12:42:18 +0530

    modified app.module to import required services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
-import { EmbeddingModule } from './embedding/embedding.module';
+import { AiModule } from './ai/ai.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { QueryModule } from './query/query.module';
+import { VectorDbModule } from './vector-db/vector-db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // makes env vars available everywhere
     }),
-    EmbeddingModule,
+    AiModule,
+    QueryModule,
+    VectorDbModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/',
